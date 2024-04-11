84079609e01510fcc0e52687ecff86ad8c4a0994
Mikee1103 <160124042+Mikee1103@users.noreply.github.com>
2024-04-11 11:47:09 +0200

    Add files via upload

---
+++ Matrici3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Matrici3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean diagonale = true;
+        int[][] m = new int[4][4];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; i++) {
+                System.out.println("inserire elemento");
+                m[i][j] = in.nextInt();
+            }
+        }
+        System.out.println(isDiagonale(m, diagonale));
+    }
+
+    private static boolean isDiagonale(int[][] m, boolean diagonale) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; i++) {
+                if (m[i][j] != m[j][i]) {
+                    diagonale = false;
+                    break;
+                }
+            }
+        }
+        return diagonale;
+    }
+}
